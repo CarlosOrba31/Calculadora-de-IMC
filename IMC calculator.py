4bc81c7f42226c1800ce929a82c7a6c5ea412ee5
CarlosOrba31 <110425557+CarlosOrba31@users.noreply.github.com>
2023-04-03 14:11:32 +0200

    Created (and finished) the calculator

---
+++ IMC calculator.py
@@ -0,0 +1,15 @@
+peso=float(input("¿Cual es tu peso en KG?: "))
+
+altura=float(input("¿Cual es tu altura?"))
+
+imc= peso/(altura * altura)
+
+print(f"Su imc es de {imc}")
+
+if imc < 18.5:
+    print("Por debajo de lo normal")
+elif imc <= 25:
+    print("Tu peso es normal")
+else:
+    print("Por encima de lo normal")
+    # TODO: write code...
